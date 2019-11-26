41b5903252f78b82aa56c70fc164f9030b8b24d2
clbro <253033679@qq.com>
2019-11-26 20:47:01 +0800

    Update ejs-helper.js

--- middlewares/ejs-helper.js
+++ middlewares/ejs-helper.js
@@ -37,7 +37,7 @@ function helper(locals) {
     }
     for (let k in o) {
       if (new RegExp(`(${k})`).test(fmt)) {
-        fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ((`00${o[k]}`).substr(`${o[k]}`).length));
+        fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ((`00${o[k]}`).substr(`${o[k]}`.length)));
       }
     }
     return fmt;
